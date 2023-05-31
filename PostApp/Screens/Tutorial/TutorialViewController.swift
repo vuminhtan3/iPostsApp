988e45e0a44d11ed406f38070021e5c1c62f2a0d
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-05-31 19:21:23 +0700

    first commit

---
+++ PostApp/Screens/Tutorial/TutorialViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TutorialViewController.swift
+//  PostApp
+//
+//  Created by Minh Tan Vu on 31/05/2023.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
